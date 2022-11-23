ed02075e9e47e60d910cfaa5ee5b3c790974759b
Matthew Austin <maustin803@gmail.com>
2022-11-23 11:33:44 -0800

    Try to get images to link correctly

--- js/app.js
+++ js/app.js
@@ -16,25 +16,25 @@ function Product(name, src) {
 
 // Create Products for all included images
 const productsArray = [
-  new Product('bag', 'img/bag.jpg'),
-  new Product('banana', 'img/banana.jpg'),
-  new Product('bathroom', 'img/bathroom.jpg'),
-  new Product('boots', 'img/boots.jpg'),
-  new Product('breakfast', 'img/breakfast.jpg'),
-  new Product('bubblegum', 'img/bubblegum.jpg'),
-  new Product('chair', 'img/chair.jpg'),
-  new Product('cthulhu', 'img/cthulhu.jpg'),
-  new Product('dog-duck', 'img/dog-duck.jpg'),
-  new Product('dragon', 'img/dragon.jpg'),
-  new Product('pen', 'img/pen.jpg'),
-  new Product('pet-sweep', 'img/pet-sweep.jpg'),
-  new Product('scissors', 'img/scissors.jpg'),
-  new Product('shark', 'img/shark.jpg'),
-  new Product('sweep', 'img/sweep.png'),
-  new Product('tauntaun', 'img/tauntaun.jpg'),
-  new Product('unicorn', 'img/unicorn.jpg'),
-  new Product('water-can', 'img/water-can.jpg'),
-  new Product('wine-glass', 'img/wine-glass.jpg')
+  new Product('bag', './img/bag.jpg'),
+  new Product('banana', './img/banana.jpg'),
+  new Product('bathroom', './img/bathroom.jpg'),
+  new Product('boots', './img/boots.jpg'),
+  new Product('breakfast', './img/breakfast.jpg'),
+  new Product('bubblegum', './img/bubblegum.jpg'),
+  new Product('chair', './img/chair.jpg'),
+  new Product('cthulhu', './img/cthulhu.jpg'),
+  new Product('dog-duck', './img/dog-duck.jpg'),
+  new Product('dragon', './img/dragon.jpg'),
+  new Product('pen', './img/pen.jpg'),
+  new Product('pet-sweep', './img/pet-sweep.jpg'),
+  new Product('scissors', './img/scissors.jpg'),
+  new Product('shark', './img/shark.jpg'),
+  new Product('sweep', './img/sweep.png'),
+  new Product('tauntaun', './img/tauntaun.jpg'),
+  new Product('unicorn', './img/unicorn.jpg'),
+  new Product('water-can', './img/water-can.jpg'),
+  new Product('wine-glass', './img/wine-glass.jpg')
 ];
 
 const resultsButton = document.getElementById('resultsButton');
